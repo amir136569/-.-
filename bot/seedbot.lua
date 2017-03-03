1acfaeb922caed28565913a78adc6dd3b9b31fe4
amir136569 <www.amir136569@gmail.com>
2017-03-03 19:02:52 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -223,10 +223,10 @@ function create_config( )
     "download_media",
     "invite",
     "all",
-    "leave_ban",
+6    "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {@Soday_eshq)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
